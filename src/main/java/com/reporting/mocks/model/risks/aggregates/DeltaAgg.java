7eee42ed03b44f4a066bd449d80437c1c452e9c4
Jason Brown <jasonbrown@fastmail.fm>
2019-05-11 17:54:49 +0200

    cleaned up directory

---
+++ src/main/java/com/reporting/mocks/model/risks/aggregates/DeltaAgg.java
@@ -0,0 +1,46 @@
+package com.reporting.mocks.model.risks.aggregates;
+
+import com.reporting.mocks.model.id.MarketEnvId;
+import com.reporting.mocks.model.id.RiskRunId;
+import com.reporting.mocks.model.risks.RiskType;
+import com.reporting.mocks.model.underlying.Underlying;
+
+public class DeltaAgg extends RiskAggregate {
+    private Underlying underlying;
+    private Double value;
+    
+    public DeltaAgg() {
+        super();
+        this.riskType = RiskType.DELTA;
+    }
+
+    public DeltaAgg(MarketEnvId marketEnvId,
+                 RiskRunId riskRunId,
+                 String bookName,
+                 Underlying currency,
+                 Double value) {
+        super(marketEnvId, riskRunId, bookName, RiskType.DELTA);
+        this.underlying = currency;
+        this.value = value;
+    }
+
+    public void addValue(Double valueToAdd) {
+        this.value += valueToAdd;
+    }
+
+    public Underlying getUnderlying() {
+        return underlying;
+    }
+
+    public void setUnderlying(Underlying underlying) {
+        this.underlying = underlying;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+}
