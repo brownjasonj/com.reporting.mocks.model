6508a3de045c11a9adb486d7d28d4ea59b3f6875
Jason Brown <jasonbrown@fastmail.fm>
2020-11-08 16:43:59 +0100

    TradeLifecycle event now has a before and after trade state to enable encoding of the two states of the lifecycle event

--- src/main/java/com/reporting/mocks/model/TradeLifecycle.java
+++ src/main/java/com/reporting/mocks/model/TradeLifecycle.java
@@ -3,16 +3,22 @@ package com.reporting.mocks.model;
 import com.reporting.mocks.model.trade.Trade;
 
 public class TradeLifecycle {
-    protected Trade trade;
+    protected Trade tradeBeforeLifeCycle;           // trade before the lifecycle
+    protected Trade tradeAfterLifeCycle;            // trade after the lifecycle
     protected TradeLifecycleType lifecycleType;
 
-    public TradeLifecycle(TradeLifecycleType type, Trade trade) {
-        this.trade = trade;
+    public TradeLifecycle(TradeLifecycleType type, Trade tradeBeforeLifeCycle, Trade tradeAfterLifeCycle) {
+        this.tradeBeforeLifeCycle = tradeBeforeLifeCycle;
+        this.tradeAfterLifeCycle = tradeAfterLifeCycle;
         this.lifecycleType = type;
     }
 
-    public Trade getTrade() {
-        return trade;
+    public Trade getTradeBeforeLifeCycle() {
+        return tradeBeforeLifeCycle;
+    }
+
+    public Trade getTradeAfterLifeCycle() {
+        return tradeAfterLifeCycle;
     }
 
     public TradeLifecycleType getLifecycleType() {
