05259c6025f04d3d3ebcffb41b620509107a5498
Jason Brown <jasonbrown@fastmail.fm>
2020-12-29 06:48:58 +0100

    - change PricingGroup to include books, needed for the new persistence service module - first implementation of the new persistence service module

--- src/main/java/com/reporting/mocks/model/PricingGroup.java
+++ src/main/java/com/reporting/mocks/model/PricingGroup.java
@@ -1,15 +1,24 @@
 package com.reporting.mocks.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PricingGroup {
     protected String name;
+    protected List<String> tradingBooks;
 
     protected PricingGroup() {}
 
-    public PricingGroup(String name) {
+    public PricingGroup(String name, List<String> tradingBooks) {
         this.name = name;
+        this.tradingBooks = new ArrayList<>(tradingBooks);
     }
 
     public String getName() {
         return name;
+    }
+
+    public List<String> getTradingBooks() {
+        return tradingBooks;
     }
 }
